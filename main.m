db51d6f3441be20ea7ea028ad2e2d7199b35ce2e
nongshifu <350722326@qq.com>
2022-09-21 00:55:48 +0800

    优化验证流程

--- main.m
+++ main.m
@@ -4,10 +4,7 @@
 +(void)load
 {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        static dispatch_once_t onceToken;
-        dispatch_once(&onceToken, ^{
-            [NSObject Bsphp];
-        });
+        [NSObject Bsphp];
     });
 }
 @end
